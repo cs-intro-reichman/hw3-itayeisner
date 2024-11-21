0348d5b2540fbb072d7bcb0001677ba16de7df99
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-11-21 23:30:54 +0200

    first anagram

--- Anagram.java
+++ Anagram.java
@@ -6,7 +6,7 @@ public class Anagram {
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
-
+ 
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
 		
@@ -23,13 +23,32 @@ public class Anagram {
 			pass = pass && isAnagram(str, randomAnagram);
 			if (!pass) break;
 		}
-		System.out.println(pass ? "test passed" : "test Failed");
+		System.out.println(pass ? "test passed" : "test Failed"); 
 	}  
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
 		// Replace the following statement with your code
-		return false;
+		boolean isang = false;
+		String str11 = preProcess(str1);
+		String str22 = preProcess(str2);
+		if(str11.length()!=str22.length()){return false;}
+		int count1 = 0;
+		int count2 = 0;
+		for(int i=0;i<str11.length();i++){
+			for(int j=0;j<str11.length();j++){
+				if(str11.charAt(i)==str11.charAt(j)){count1++;}
+			}
+			for(int j=0;j<str22.length();j++){
+				if(str11.charAt(i)==str22.charAt(j)){count2++;}
+			}
+			if(count1==count2){isang=true;}
+			else{return false;}
+			count1 = 0;
+			count2 = 0;
+		}
+		
+		return isang;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
@@ -37,13 +56,34 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+		String pre = "";
+		for(int i=0;i<str.length();i++){
+			if(str.charAt(i)>='a'&&str.charAt(i)<='z'||str.charAt(i)>='A'&&str.charAt(i)<='Z'){
+				pre += str.charAt(i);
+			}
+		}
+		pre = pre.toLowerCase();
+		return pre;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
-	}
+		String sub = "";
+		int len = str.length();
+		boolean isan = false;
+		while(!isan){
+			for(int i=0;i<str.length();i++){
+				int num = (int) (Math.random()*len);
+				if(sub.indexOf(str.charAt(num))==-1){
+				sub += str.charAt(num);}
+			
+			}
+			if(isAnagram(sub, str)){isan = true;}
+		}
+		
+		return sub;	
+		}
+		
 }
